f1f2fe2f85aa6cee970ced8b693f742bb21b29fc
PylenkovIvan <ivanvan906@yandex.ru>
2026-05-11 02:30:41 +0300

    Add files via upload

--- clone_among_us_bot.py
+++ clone_among_us_bot.py
@@ -97,12 +97,6 @@ async def start(update, context):
 
 
 async def help_command(update, context):
-    reply_keyboard = [['/help', '/active_tasks'],
-                      ['/complete_task', '/give_file'],
-                      ['/emergency_meeting'],
-                      ['/finish'],
-                      ['/crash']]
-    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
     await update.message.reply_text('Команды для работы с бортовым компьютером: \n'
                                     '1. "/active_tasks" - выведет список невыполненных заданий \n'
                                     '2. "/complete_task" - запросит номер выполненного задания и '
@@ -123,7 +117,7 @@ async def help_command(update, context):
                                     'Если вы являетесь инженером, то после выполнения всех своих'
                                     ' заданий \n'
                                     'активируйте задание номер 100 в /complete_task \n',
-                                    reply_markup=markup)
+                                    reply_markup=get_main_keyboard())
 
 
 async def active_tasks(update, context):
@@ -132,15 +126,9 @@ async def active_tasks(update, context):
     global crash_fl
     if not crash_fl:
         tasks = ''
-        reply_keyboard = [['/help', '/active_tasks'],
-                          ['/complete_task', '/give_file'],
-                          ['/emergency_meeting'],
-                          ['/finish'],
-                          ['/crash']]
-        markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
         for el in active_tasks_array:
             tasks += f'Задание №{el}: {d_of_tasks[el]} \n'
-        await update.message.reply_text(tasks, reply_markup=markup)
+        await update.message.reply_text(tasks, reply_markup=get_main_keyboard())
     if crash_fl:
         await update.message.reply_text('Сбои в системе. Команда не принята')
 
@@ -175,22 +163,10 @@ async def enter_task(update, context):
                 fl = False
         if fl:
             task_number = int(update.message.text)
-        reply_keyboard = [['/help', '/active_tasks'],
-                          ['/complete_task', '/give_file'],
-                          ['/emergency_meeting'],
-                          ['/finish'],
-                          ['/crash']]
-        markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
         if task_number == 100:
             if not fl_enter:
-                reply_keyboard = [['/help', '/active_tasks'],
-                                  ['/complete_task', '/give_file'],
-                                  ['/emergency_meeting'],
-                                  ['/finish'],
-                                  ['/crash']]
-                markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
                 await update.message.reply_text('Активируйте команду /complete_task',
-                                                reply_markup=markup)
+                                                reply_markup=get_main_keyboard())
             else:
                 fl_enter = False
                 for i in range(len(active_tasks_array)):
@@ -201,22 +177,13 @@ async def enter_task(update, context):
                         del failed_tasks[failed_tasks.index(task_number[0])]
                     if len(active_tasks_array) == 0:
                         text += '\nВсе задания выполнены!'
-                        reply_keyboard = [['/start']]
-                        markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
                         restart_game()
-                    await update.message.reply_text(text,
-                                                    reply_markup=markup)
+                    await update.message.reply_text(text, reply_markup=get_main_keyboard())
                     await asyncio.sleep(0.1)
         elif task_number in active_tasks_array:
             if not fl_enter:
-                reply_keyboard = [['/help', '/active_tasks'],
-                                  ['/complete_task', '/give_file'],
-                                  ['/emergency_meeting'],
-                                  ['/finish'],
-                                  ['/crash']]
-                markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
                 await update.message.reply_text('Активируйте команду /complete_task',
-                                                reply_markup=markup)
+                                                reply_markup=get_main_keyboard())
             else:
                 fl_enter = False
                 n = random.randint(1, 20)
@@ -229,17 +196,14 @@ async def enter_task(update, context):
                         text += f' Можно выполнить задание №{can_complete}'
                     if len(active_tasks_array) == 0:
                         text += '\nВсе задания выполнены!'
-                        reply_keyboard = [['/start']]
-                        markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
                         restart_game()
-                    await update.message.reply_text(text,
-                                                    reply_markup=markup)
+                    await update.message.reply_text(text, reply_markup=get_main_keyboard())
                 if n == 7 and task_number not in failed_tasks:
                     if len(failed_tasks) < len(active_tasks_array) // 2:
                         failed_tasks.append(task_number)
                         await update.message.reply_text(
                             f'Задание не выполнено. Оно не прошло проверку...',
-                            reply_markup=markup)
+                            reply_markup=get_main_keyboard())
                     else:
                         del active_tasks_array[active_tasks_array.index(task_number)]
                         text = f'Задание №{task_number} выполнено!'
@@ -249,32 +213,23 @@ async def enter_task(update, context):
                             text += f' Можно выполнить задание №{can_complete}'
                         if len(active_tasks_array) == 0:
                             text += '\nВсе задания выполнены!'
-                            reply_keyboard = [['/start']]
-                            markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
                             restart_game()
-                        await update.message.reply_text(text,
-                                                        reply_markup=markup)
+                        await update.message.reply_text(text, reply_markup=get_main_keyboard())
                 elif task_number in failed_tasks:
                     await update.message.reply_text('Задание пока нельзя выполнить',
-                                                    reply_markup=markup)
+                                                    reply_markup=get_main_keyboard())
         elif task_number not in active_tasks_array and task_number in range(1, 37):
             if not fl_enter:
-                reply_keyboard = [['/help', '/active_tasks'],
-                                  ['/complete_task', '/give_file'],
-                                  ['/emergency_meeting'],
-                                  ['/finish'],
-                                  ['/crash']]
-                markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
                 await update.message.reply_text('Активируйте команду /complete_task',
-                                                reply_markup=markup)
+                                                reply_markup=get_main_keyboard())
             else:
                 fl_enter = False
                 await update.message.reply_text('Задание было выполнено до вас',
-                                                reply_markup=markup)
+                                                reply_markup=get_main_keyboard())
         else:
             fl_enter = False
             await update.message.reply_text('Бортовой компьютер не разобрал вашего сообщения',
-                                            reply_markup=markup)
+                                            reply_markup=get_main_keyboard())
 
 
 async def finish(update, context):
@@ -292,51 +247,17 @@ async def finish(update, context):
 
 async def crash(update, context):
     global crash_fl
-    chat_id = update.message.chat_id
-    job_removed = remove_job_if_exists(str(chat_id), context)
-    context.job_queue.run_once(task, TIMER, chat_id=chat_id, name=str(chat_id), data=TIMER)
+    chat_id = update.effective_chat.id
+    remove_job_if_exists(str(chat_id), context)
+    context.job_queue.run_once(task, TIMER, chat_id=chat_id, name=str(chat_id))
     crash_fl = True
     await update.message.reply_text("Всё сломалось, помогите!", reply_markup=ReplyKeyboardRemove())
 
 
 async def meeting(update, context):
     global crash_fl
-    reply_keyboard = [['/help', '/active_tasks'],
-                      ['/complete_task', '/give_file'],
-                      ['/emergency_meeting'],
-                      ['/finish'],
-                      ['/crash']]
-    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
     if not crash_fl:
-        await update.message.reply_text("Собрание", reply_markup=markup)
-    if crash_fl:
-        await update.message.reply_text('Сбои в системе. Команда не принята')
-
-
-async def first_level(update, context):
-    global crash_fl
-    reply_keyboard = [['/help', '/active_tasks'],
-                      ['/complete_task', '/give_file'],
-                      ['/emergency_meeting'],
-                      ['/finish'],
-                      ['/crash']]
-    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
-    if not crash_fl:
-        await update.effective_message.reply_photo('first_level.jpg', reply_markup=markup)
-    if crash_fl:
-        await update.message.reply_text('Сбои в системе. Команда не принята')
-
-
-async def second_level(update, context):
-    global crash_fl
-    reply_keyboard = [['/help', '/active_tasks'],
-                      ['/complete_task', '/give_file'],
-                      ['/emergency_meeting'],
-                      ['/finish'],
-                      ['/crash']]
-    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
-    if not crash_fl:
-        await update.effective_message.reply_photo('second_level.jpg', reply_markup=markup)
+        await update.message.reply_text("Собрание", reply_markup=get_main_keyboard())
     if crash_fl:
         await update.message.reply_text('Сбои в системе. Команда не принята')
 
@@ -345,12 +266,6 @@ async def fix_errors(update, context):
     global crash_fl
     global active_tasks_array
     crash_fl = False
-    reply_keyboard = [['/help', '/active_tasks'],
-                      ['/complete_task', '/give_file'],
-                      ['/emergency_meeting'],
-                      ['/finish'],
-                      ['/crash']]
-    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
     failed_tasks_after_crash = list()
     for i in range(1, 37):
         if i not in active_tasks_array:
@@ -373,7 +288,7 @@ async def fix_errors(update, context):
                             'Тут такое бы произошло! \n' \
                             'С кем я на корабле... \n' \
                             'Остановите, я выйду...'
-    await update.message.reply_text(text, reply_markup=markup)
+    await update.message.reply_text(text, reply_markup=get_main_keyboard())
 
 
 async def task(context):
@@ -384,14 +299,18 @@ async def task(context):
     restart_game()
     reply_keyboard = [['/start']]
     markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
-    await context.bot.send_message(context.job.chat_id, text=f'Критическое повреждение системы. '
-                                                             f'Дальнейшее управление невозможно. '
-                                                             f'Игра завершена...',
+    await context.bot.send_message(chat_id=context.job.chat_id,
+                                   text='Критическое повреждение системы. '
+                                        'Дальнейшее управление невозможно. '
+                                        'Игра завершена...',
                                    reply_markup=markup)
 
 
 async def text_buttons(update, context):
     text = update.message.text
+
+    if text.startswith('/'):
+        return
 
     if text == 'Помощь':
         await help_command(update, context)
